ef66d06bcd83c87bef861d3dc382c830ea6c6428
Joe Rogers <fjrogers@paciolan.com>
2025-12-21 15:27:28 -0800

    Add filter to remove null users being sent to WhiteboardCard

--- Frontend/src/components/WhiteboardList.tsx
+++ Frontend/src/components/WhiteboardList.tsx
@@ -80,15 +80,20 @@ const WhiteboardList = (props: WhiteboardListProps): React.JSX.Element => {
               className="flex flex-col md:flex-row justify-center flex-wrap content-center"
             >
             {
-                whiteboardsAttribs.map(attribs => (
+              whiteboardsAttribs
+                .map(attribs => (
                   <li
                     key={attribs.id}
                   >
                     <WhiteboardCard
                       {...attribs}
+                      user_permissions={attribs.user_permissions.filter(up =>
+                        up != null &&
+                        ("user" in up ? up.user !== null : up.email !== null)
+                      )}
                     />
                   </li>
-                ))
+              ))
             }
             </ul>
           );
