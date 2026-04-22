28789dfbfb2661adabb348c2e8c0f3c645583a3a
Oliver A. Kelton <oakelton@gmail.com>
2026-04-21 21:01:10 -0700

    deploy and teardown scripts set current working directory to deployment.d, to ensure consistent behavior no matter where the script is called from.

--- deployment.d/deploy.sh
+++ deployment.d/deploy.sh
@@ -12,9 +12,12 @@
 # 
 # ==============================================================================
 
+# -- Change working directory to deployment.d
+cd "$(dirname "$0")"
+
 # -- Source environment variables from .env
 set -a
-source .env
+source ../.env
 
 # -- Ensure kind is running
 if [[ $(kind get clusters | wc -l) -lt 1 ]]
@@ -28,29 +31,29 @@ kind load docker-image "whiteboard_editor/rest_api:latest"
 kind load docker-image "whiteboard_editor/web_socket_server:latest"
 
 # -- Set up namespaces
-kubectl apply -f <(envsubst < deployment.d/namespaces.yml)
+kubectl apply -f <(envsubst < namespaces.yml)
 
 # -- Set up secrets
-kubectl -n whiteboard-editor create secret generic whiteboard-editor-config --from-env-file .env
+kubectl -n whiteboard-editor create secret generic whiteboard-editor-config --from-env-file ../.env
 
 # -- Deploy frontend pods
-kubectl apply -f <(envsubst < deployment.d/frontend_deployment.yml)
+kubectl apply -f <(envsubst < frontend_deployment.yml)
 
 # -- Deploy frontend service
-kubectl apply -f <(envsubst < deployment.d/frontend_service.yml)
+kubectl apply -f <(envsubst < frontend_service.yml)
 
 # -- Deploy rest_api pods
-kubectl apply -f <(envsubst < deployment.d/rest_api_deployment.yml)
+kubectl apply -f <(envsubst < rest_api_deployment.yml)
 
 # -- Deploy rest_api service
-kubectl apply -f <(envsubst < deployment.d/rest_api_service.yml)
+kubectl apply -f <(envsubst < rest_api_service.yml)
 
 # -- Deploy web_socket_server pods
-kubectl apply -f <(envsubst < deployment.d/web_socket_server_deployment.yml)
+kubectl apply -f <(envsubst < web_socket_server_deployment.yml)
 
 # -- Deploy web_socket_server service
-kubectl apply -f <(envsubst < deployment.d/web_socket_server_service.yml)
+kubectl apply -f <(envsubst < web_socket_server_service.yml)
 
 # -- Implement gateway
-kubectl apply -f <(envsubst < deployment.d/gateway.yml)
-kubectl apply -f <(envsubst < deployment.d/http-routes.yml)
+kubectl apply -f <(envsubst < gateway.yml)
+kubectl apply -f <(envsubst < http-routes.yml)
--- deployment.d/teardown.sh
+++ deployment.d/teardown.sh
@@ -1,33 +1,36 @@
 #!/bin/bash -v
 
+# -- Change working directory to deployment.d
+cd "$(dirname "$0")"
+
 # -- Source environment variables from .env
 set -a
-source .env
+source ../.env
 
 # -- Delete gateway
-kubectl delete -f <(envsubst < deployment.d/http-routes.yml)
-kubectl delete -f <(envsubst < deployment.d/gateway.yml)
+kubectl delete -f <(envsubst < http-routes.yml)
+kubectl delete -f <(envsubst < gateway.yml)
 
 # -- Delete frontend pods
-kubectl delete -f <(envsubst < deployment.d/frontend_deployment.yml)
+kubectl delete -f <(envsubst < frontend_deployment.yml)
 
 # -- Delete frontend service
-kubectl delete -f <(envsubst < deployment.d/frontend_service.yml)
+kubectl delete -f <(envsubst < frontend_service.yml)
 
 # -- Delete rest_api pods
-kubectl delete -f <(envsubst < deployment.d/rest_api_deployment.yml)
+kubectl delete -f <(envsubst < rest_api_deployment.yml)
 
 # -- Delete rest_api service
-kubectl delete -f <(envsubst < deployment.d/rest_api_service.yml)
+kubectl delete -f <(envsubst < rest_api_service.yml)
 
 # -- Delete web_socket_server pods
-kubectl delete -f <(envsubst < deployment.d/web_socket_server_deployment.yml)
+kubectl delete -f <(envsubst < web_socket_server_deployment.yml)
 
 # -- Delete web_socket_server service
-kubectl delete -f <(envsubst < deployment.d/web_socket_server_service.yml)
+kubectl delete -f <(envsubst < web_socket_server_service.yml)
 
 # -- Remove secrets
 kubectl -n whiteboard-editor delete secret whiteboard-editor-config
 
 # -- Remove namespaces
-kubectl delete -f <(envsubst < deployment.d/namespaces.yml)
+kubectl delete -f <(envsubst < namespaces.yml)
