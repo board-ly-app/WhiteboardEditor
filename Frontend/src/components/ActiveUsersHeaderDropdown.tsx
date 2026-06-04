4ee88bf10addb1f5eec6dc2451fe5eb4f083cfd8
joerogers12 <joerogers212@gmail.com>
2026-06-03 20:50:26 -0700

    Implement dropdown for overflow user icons

--- Frontend/src/components/ActiveUsersHeaderDropdown.tsx
+++ Frontend/src/components/ActiveUsersHeaderDropdown.tsx
@@ -1,5 +1,5 @@
 import {
-  // useState,
+  useState,
   useContext,
 } from 'react';
 
@@ -9,10 +9,10 @@ import {
   useSelector,
 } from 'react-redux';
 
-// import {
-//   ChevronDown,
-//   Circle,
-// } from 'lucide-react';
+import {
+  ChevronDown,
+  Circle,
+} from 'lucide-react';
 
 import {
   type ClientIdType,
@@ -32,12 +32,14 @@ import {
 
 import WhiteboardContext from '@/context/WhiteboardContext';
 
-// import {
-//   DropdownMenu,
-//   DropdownMenuContent,
-//   DropdownMenuLabel,
-//   DropdownMenuTrigger,
-// } from '@/components/ui/dropdown-menu';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+const ACTIVE_USERS_LIMIT = 2;
 
 export const ActiveUsersHeaderDropdown = () => {
   // TODO: Abstract out a generic dropdown menu
@@ -52,55 +54,73 @@ export const ActiveUsersHeaderDropdown = () => {
     whiteboardId,
   } = whiteboardContext;
 
-  // const [isActiveUsersOpen, setIsActiveUsersOpen] = useState<boolean>(false);
+  const [isActiveUsersOpen, setIsActiveUsersOpen] = useState<boolean>(false);
 
   const activeUsers : Record<ClientIdType, ClientSummary> = useSelector(
     (state: RootState) => selectActiveUsersByWhiteboard(state, whiteboardId) || null,
     lodash.isEqual
   );
 
-  return (
-    <div className="flex items-center gap-1">
-      {activeUsers && Object.values(activeUsers).map((u) => (
-        <div
-          key={u.clientId}
-          className="w-8 h-8 rounded-full flex items-center justify-center text-sm font-semibold text-white select-none"
-          style={{ backgroundColor: u.color }}
-          title={u.username}
-        >
-          {u.username[0].toUpperCase()}
-        </div>
-      ))}
-    </div>
+  let activeUsersLength =  Object.keys(activeUsers).length;
 
-    // <DropdownMenu
-    //   key="active-users"
-    //   open={isActiveUsersOpen}
-    //   onOpenChange={setIsActiveUsersOpen}
-    // >
-    //   <DropdownMenuTrigger className="text-header-button-text group flex items-center gap-1 px-4 py-2 rounded-lg hover:cursor-pointer hover:text-header-button-text-hover whitespace-nowrap">
-    //     Active Users
-    //     <ChevronDown className="w-4 h-4 transition-transform duration-300 group-data-[state=open]:rotate-180"/>
-    //   </DropdownMenuTrigger>
-    //   <DropdownMenuContent>
-    //     <div className="flex flex-col">
-    //       {activeUsers && Object.values(activeUsers).map((u) => (
-    //         <DropdownMenuLabel
-    //           key={u.clientId}
-    //           className="flex flex-row content-center"
-    //         >
-    //           <Circle
-    //             size={20}
-    //             stroke={u.color}
-    //             strokeWidth={4}
-    //           />
-    //           <span className="pl-2">
-    //             {u.username}
-    //           </span>
-    //         </DropdownMenuLabel>
-    //       ))}
-    //     </div>
-    //   </DropdownMenuContent>
-    // </DropdownMenu>
+  return (
+    activeUsersLength < ACTIVE_USERS_LIMIT
+      ? 
+      // Display all user icons side-by-side on header
+      <div className="flex items-center gap-1">
+        {activeUsers && Object.values(activeUsers).map((u) => (
+          <div
+            key={u.clientId}
+            className="w-8 h-8 rounded-full flex items-center justify-center text-sm font-semibold text-white select-none"
+            style={{ backgroundColor: u.color }}
+            title={u.username}
+          >
+            {u.username[0].toUpperCase()}
+          </div>
+        ))}
+      </div>
+      : 
+      // Display first <ACTIVE_USERS_LIMIT> user icons on header with dropdown option to see all
+      <DropdownMenu
+        key="active-users"
+        open={isActiveUsersOpen}
+        onOpenChange={setIsActiveUsersOpen}
+      >
+        <div className='flex justify-center items-center gap-2'>
+          {activeUsers && Object.values(activeUsers).slice(0, ACTIVE_USERS_LIMIT - 1).map((u) => (
+            <div
+              key={u.clientId}
+              className="w-8 h-8 rounded-full flex items-center justify-center text-sm font-semibold text-white select-none"
+              style={{ backgroundColor: u.color }}
+              title={u.username}
+            >
+              {u.username[0].toUpperCase()}
+            </div>
+          ))}
+          <DropdownMenuTrigger className="text-header-button-text group flex items-center gap-1 px-0 py-2 rounded-lg hover:cursor-pointer hover:text-header-button-text-hover whitespace-nowrap">
+            ...
+            <ChevronDown className="w-4 h-4 transition-transform duration-300 group-data-[state=open]:rotate-180"/>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <div className="flex flex-col">
+              {activeUsers && Object.values(activeUsers).map((u) => (
+                <DropdownMenuLabel
+                  key={u.clientId}
+                  className="flex flex-row content-center"
+                >
+                  <Circle
+                    size={20}
+                    stroke={u.color}
+                    strokeWidth={4}
+                  />
+                  <span className="pl-2">
+                    {u.username}
+                  </span>
+                </DropdownMenuLabel>
+              ))}
+            </div>
+          </DropdownMenuContent>
+        </div>
+      </DropdownMenu>
   );
 };// -- end ActiveUsersHeaderDropdown
