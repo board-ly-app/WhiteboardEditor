84e53d9e4e809792c028dc819540a70dcff7c757
Oliver A. Kelton <oakelton@gmail.com>
2026-06-07 13:05:13 -0700

    Created global rate limiter, applied to all routes.

--- RestAPI/src/routes/auth.ts
+++ RestAPI/src/routes/auth.ts
@@ -1,7 +1,14 @@
 import { Router } from "express";
 import { login } from "../controllers/auth";
 
+import {
+  globalRateLimiter,
+} from '../middleware/rateLimit';
+
 const router = Router();
+
+// -- apply rate limiting
+router.use(globalRateLimiter);
 
 router.post("/login", login);
 
