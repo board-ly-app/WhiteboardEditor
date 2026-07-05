091e047fef1f1600ffad8e8d2a961540a694976d
Oliver A. Kelton <oakelton@gmail.com>
2026-07-05 14:04:22 -0700

    Added debug logging to web socket server message handler.

--- WebSocketServer/src/wss/server.rs
+++ WebSocketServer/src/wss/server.rs
@@ -828,7 +828,9 @@ pub async fn handle_authenticated_client_message<'a>(
 
                     // -- make sure client is either a whiteboard owner or already has edit access
                     // to the canvas
+                    eprintln!("!! CHECKING WHITEBOARD PERMISSIONS");
                     if ! matches!(whiteboard.metadata().permission_for_user(&client_state.user_summary.user_id), Some(WhiteboardPermissionEnum::Own)) {
+                        eprintln!("!! NO OWNER PERMISSION; CHECKING CANVAS PERMISSIONS");
                         let canvas : &Canvas = whiteboard.canvases().get(&canvas_id).unwrap();
 
                         if let Some(curr_allowed_users) = canvas.allowed_users() {
