b2eadf7e4a2cc3f73c70c49f274a41526025577e
Oliver A. Kelton <oakelton@gmail.com>
2026-07-16 22:07:07 -0700

    Implemented /auth/logout endpoint to clear refresh token cookie.

--- RestAPI/src/routes/auth.ts
+++ RestAPI/src/routes/auth.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   handleLogin,
+  handleLogout,
   handleRefreshAccessToken,
 } from "../controllers/auth";
 
@@ -16,6 +17,9 @@ router.use(globalRateLimiter);
 // -- Authenticate with username/email and password, receiving both refresh
 // token and access token
 router.post("/login", handleLogin);
+
+// -- Clear cookies, including refresh token
+router.post('/logout', handleLogout);
 
 // -- Request a new access token, given a valid refresh token.
 router.post('/refresh', handleRefreshAccessToken);
