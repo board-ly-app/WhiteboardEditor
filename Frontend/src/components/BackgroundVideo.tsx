434adc542c0aa64e57553e5d7247f09b5b8090dc
joerogers12 <joerogers212@gmail.com>
2026-07-19 17:49:30 -0700

    Add vignette to video

--- Frontend/src/components/BackgroundVideo.tsx
+++ Frontend/src/components/BackgroundVideo.tsx
@@ -2,18 +2,22 @@ import backgroundVideo from '@/assets/backgroundVideo.mp4';
 
 const BackgroundVideo = () => {
   return(
-    <div>
-      <video 
+    <div className="fixed inset-0 -z-10 h-full w-full overflow-hidden">
+  
+      {/* Layer 1: Background Video */}
+      <video
         id="backgroundVideo"
         autoPlay
-        muted
         loop
+        muted
         playsInline
-        className='fixed inset-0 -z-10 h-full w-full object-cover'
+        className="absolute inset-0 h-full w-full object-cover"
       >
         <source src={backgroundVideo} type="video/mp4" />
-        Your browser does not support HTML5 video.
       </video>
+
+      {/* Layer 2: The Vignette Overlay */}
+      <div className="absolute inset-0 pointer-events-none bg-[radial-gradient(circle,transparent_50%,rgba(0,0,0,0.6)_120%)]"></div>
     </div>
   );
 }
