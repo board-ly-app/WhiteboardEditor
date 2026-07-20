8f0ad3c470ceadd8ff444b56025f09ef48688bef
joerogers12 <joerogers212@gmail.com>
2026-07-19 17:52:04 -0700

    Stick footer to bottom of page

--- Frontend/src/pages/Dashboard.tsx
+++ Frontend/src/pages/Dashboard.tsx
@@ -310,7 +310,7 @@ const Dashboard = (): React.JSX.Element => {
         title={APP_NAME}
       />
 
-      <main>
+      <main className='flex-1'>
         <div className="grid grid-flow-col grid-rows-3 sm:grid-rows-2 lg:grid-rows-1 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mx-12 mb-8">
           <div className='col-span-1 order-2 lg:order-1 flex justify-center sm:justify-start'>
             <CreateWhiteboardModal
