389920e64fee157382cc0a705bd6664ef6768126
Oliver A. Kelton <oakelton@gmail.com>
2026-07-26 15:01:35 -0700

    Added fallback image to background video.

--- Frontend/src/components/BackgroundVideo.tsx
+++ Frontend/src/components/BackgroundVideo.tsx
@@ -11,8 +11,10 @@ const BackgroundVideo = () => {
         loop
         muted
         playsInline
+        poster="https://upload.wikimedia.org/wikipedia/commons/5/5e/Bob_Saget%2C_Behind_The_Velvet_Rope_TV_.05.jpg"
         className="absolute inset-0 h-full w-full object-cover"
       >
+        {/** This should only fail if you're a weirdo who uses Fedora; in that case, you get a picture of Bob Saget as a fallback **/}
         <source src={backgroundVideo} type="video/mp4" />
       </video>
 
